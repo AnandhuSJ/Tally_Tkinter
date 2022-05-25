570489ec14e23a92dcd8eed8ec7638da918fb86a
Anandhu <anandhusj864@gmail.com>
2022-05-25 23:04:34 +0530

    ninth commit

--- Tally-Tkinter.py
+++ Tally-Tkinter.py
@@ -363,6 +363,131 @@ def daybook():
 
 
 
+# side buttons 
+    side1 = Button(top, text="  Period",activeforeground="black", activebackground="white",
+            fg='black', bg='#f0f8fc', borderwidth=1, font=('Arial 10  '),anchor='w').place(x=1224, y=63,height=27,width=138)
+    side2 = Button(top, text="  Company",activeforeground="black", activebackground="white",
+            fg='black', bg='#f0f8fc', borderwidth=1, font=('Arial 10 '),anchor='w').place(x=1224, y=91,height=27,width=138)
+    side3 = Button(top, text="  Group",activeforeground="black", activebackground="white",
+            fg='black', bg='#f0f8fc', borderwidth=1, font=('Arial 10 '),anchor='w').place(x=1224, y=120,height=27,width=138) 
+
+    side4 = Button(top, text="  Ledger-wise",activeforeground="black", activebackground="white",
+             fg='black', bg='#f0f8fc', borderwidth=1, font=('Arial 10 '),anchor='w').place(x=1224, y=165,height=27,width=138)
+    side5 = Button(top, text="  Monthly",activeforeground="black", activebackground="white",
+             fg='black', bg='#f0f8fc', borderwidth=1, font=('Arial 10 '),anchor='w').place(x=1224, y=194,height=27,width=138)
+    side6 = Button(top, text="  ",activeforeground="black", activebackground="white",
+             fg='black', bg='#f0f8fc', borderwidth=1, font=('Arial 10 '),anchor='w').place(x=1224, y=223,height=27,width=138) 
+    side7 = Button(top, text="  ",activeforeground="black", activebackground="white",
+             fg='black', bg='#f0f8fc', borderwidth=1, font=('Arial 10 '),anchor='w').place(x=1224, y=251,height=27,width=138) 
+    side9 = Button(top, text="  ",activeforeground="black", activebackground="white",
+             fg='black', bg='#f0f8fc', borderwidth=1, font=('Arial 10 '),anchor='w').place(x=1224, y=280,height=27,width=138)
+    side10 = Button(top, text="  ",activeforeground="black", activebackground="white",
+             fg='black', bg='#f0f8fc', borderwidth=1, font=('Arial 10 '),anchor='w').place(x=1224, y=309,height=27,width=138)
+
+    side11 = Button(top, text="  Basis of Values",activeforeground="black", activebackground="white",
+             fg='black', bg='#f0f8fc', borderwidth=1, font=('Arial 10 '),anchor='w').place(x=1224, y=350,height=27,width=138)
+    side12 = Button(top, text="  Change View",activeforeground="black", activebackground="white",
+             fg='black', bg='#f0f8fc', borderwidth=1, font=('Arial 10 '),anchor='w').place(x=1224, y=378,height=27,width=138)
+    side12 = Button(top, text="  Exception Reports",activeforeground="black", activebackground="white",
+             fg='black', bg='#f0f8fc', borderwidth=1, font=('Arial 10 '),anchor='w').place(x=1224, y=406,height=27,width=138)
+    side13 = Button(top, text="  Save View",activeforeground="black", activebackground="white",
+             fg='black', bg='#f0f8fc', borderwidth=1, font=('Arial 10 '),anchor='w').place(x=1224, y=434,height=27,width=138)
+
+    side14 = Button(top, text="  New Column",activeforeground="black", activebackground="white",
+             fg='black', bg='#f0f8fc', borderwidth=1, font=('Arial 10 '),anchor='w').place(x=1224, y=480,height=27,width=138)
+    side15 = Button(top, text="  Alter Column",activeforeground="black", activebackground="white",
+             fg='black', bg='#f0f8fc', borderwidth=1, font=('Arial 10 '),anchor='w').place(x=1224, y=508,height=27,width=138)
+    side16 = Button(top, text="  Delete Column",activeforeground="black", activebackground="white",
+             fg='#cccccc', bg='#f0f8fc', borderwidth=1, font=('Arial 10 '),anchor='w').place(x=1224, y=536,height=27,width=138)
+    side16 = Button(top, text="  Auto Column",activeforeground="black", activebackground="white",
+             fg='black', bg='#f0f8fc', borderwidth=1, font=('Arial 10 '),anchor='w').place(x=1224, y=564,height=27,width=138)
+
+    # particulars
+
+    global selected_ledgers_frame
+    selected_ledgers_frame=Frame(top,bg="white",relief=RAISED,bd=0)
+    selected_ledgers_frame.place(x=1, y=73,width=1219, height=604)
+    
+    f11=Frame(selected_ledgers_frame,bg="white",relief=RAISED,bd=0.5)
+    f11.grid(row=1,column=0,columnspan=3,ipadx=440,ipady=5)
+    l1f1=Label(f11,text="    P e r t i c u l a r s",font=("Arial",11),fg="black",bg="white",anchor="w", borderwidth=0,relief=GROOVE,width=10,height=4)
+    l1f1.pack(fill=X,pady=2,padx=2)
+
+    f12=Frame(selected_ledgers_frame,bg="white",relief=RAISED,bd=0.5)
+    f12.place(x=978,y=0,width=241,height=40)
+    l1f2=Label(f12,text="Company Name",font=("Arial",9,"bold"),bg="white",fg="black")
+    l1f2.place(x=120,y=9,anchor="center")
+    l1f3=Label(f12,text="1-1-1235",font=("Arial",8),bg="white",fg="black")
+    l1f3.place(x=95,y=27,anchor="w")
+
+    f111=Frame(selected_ledgers_frame,bg="white",relief=RAISED,bd=0.5)
+    f111.place(x=978,y=40,width=241,height=23)
+    l1f2=Label(f111,text="Closing Balance",font=("Arial",9,"bold"),bg="white",fg="black",)
+    l1f2.place(x=120,y=9,anchor="center")
+
+    f14=Frame(selected_ledgers_frame,bg="white",relief=RAISED,bd=1)
+    f14.place(x=978,y=63,width=120,height=23)
+    l1f5=Label(f14,text="Debit",font=("Arial",9),bg="white",fg="black")
+    l1f5.place(x=40,y=10,anchor="w")
+    f15=Frame(selected_ledgers_frame,bg="white",relief=RAISED,bd=1)
+    f15.place(x=1099,y=63,width=120,height=23)
+    l2f5=Label(f15,text="Credit",font=("Arial",9),bg="white",fg="black")
+    l2f5.place(x=40,y=10,anchor="w")
+
+    frame = Frame(selected_ledgers_frame.master, width = 845,bd=0)
+    frame.place(x = 2, y = 160,width=1219)
+    style = ttk.Style()
+    style.configure(".",rowheight=20, font=('Helvetica', 8), foreground="red")
+    style.configure("Treeview",foreground='Black',font=('Arial', 10,"bold"))
+    style.configure("Treeview.Heading", foreground='green',bd=0)
+    style.map('Treeview', background=[('selected', '#ffbe23')])
+    style.map('Treeview', foreground=[('selected', 'black')])
+
+    tabletrialbalance = ttk.Treeview(frame)
+    tabletrialbalance["columns"] = ("Perticulars", "Debit", "Credit")
+    tabletrialbalance.column('#0', width=0, stretch=NO)
+    tabletrialbalance.column("Perticulars", width=976)
+    tabletrialbalance.column("Debit", width=120)
+    tabletrialbalance.column("Credit", width=120)
+    tabletrialbalance.heading('#0', text='', anchor=CENTER)
+    tabletrialbalance.heading("Perticulars",)
+    tabletrialbalance.heading("Debit",anchor=W)
+    tabletrialbalance.heading("Credit",anchor=CENTER)
+    tabletrialbalance.bind('<Double-1>', groupsummry) 
+    tabletrialbalance.insert("","end",values = ("Capital Account","6746.00","45676.00"))
+    tabletrialbalance.insert("","end",values = ("Current Liabilities","7844.00","4567637"))
+    tabletrialbalance.insert("","end",values = ("Current Assets","85246.00","74185.00"))
+    tabletrialbalance.pack()
+
+    f16=Frame(selected_ledgers_frame,bg="white",relief=RAISED,)
+    f16.place(x=978,y=145,width=120,height=430)
+    f17=Frame(selected_ledgers_frame,bg="white",relief=RAISED,)
+    f17.place(x=1099,y=145,width=120,height=430)
+    b8s = Label(top,text = "Differnce in opening balance", activeforeground = "black", activebackground = "#ffbe23",fg='black',bg='white',borderwidth=0,font=('Arial  9 italic'),anchor="w")
+    b8s.place(x=0,y=630,width=980,height=18)
+    l3f6=Label(f16,text="741424.00",font=("Arial",10),bg="white",fg="black")
+    l3f6.place(x=0,y=410,anchor="nw")
+    l4f6=Label(f17,text="12146560.00",font=("Arial",10),bg="white",fg="black")
+    l4f6.place(x=0,y=410,anchor="nw")
+
+    f18=Frame(selected_ledgers_frame,bg="white",relief=RAISED,bd=1)
+    f18.place(x=0,y=579,width=977,height=25)
+    l5f6=Label(f18,text=" G r a n d  T o t a l",font=("Arial",11,"bold"),bg="white",fg="black",borderwidth=0)
+    l5f6.place(x=0,y=0,anchor="nw")
+    f19=Frame(selected_ledgers_frame,bg="white",relief=RAISED,bd=1)
+    f19.place(x=978,y=579,width=120,height=25)
+    l6f6=Label(f19,text="544650.00",font=("Arial",11,"bold"),bg="white",fg="black",borderwidth=0)
+    l6f6.place(x=0,y=0,anchor="nw")
+    f20=Frame(selected_ledgers_frame,bg="white",relief=RAISED,bd=1)
+    f20.place(x=1099,y=579,width=120,height=25)
+    l7f6=Label(f20,text="544650.00",font=("Arial",11,"bold"),bg="white",fg="black",borderwidth=0)
+    l7f6.place(x=0,y=0,anchor="nw")
+
+
+
+
+
+
 
 # NavBar Start
 name = Label(top, text="TallyPrime", fg='pink', bg='#3a646b', font=(
