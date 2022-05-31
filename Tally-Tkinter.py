8835e069d4b9ec194a5868d140ab9ca2f2e1733b
Anandhu <anandhusj864@gmail.com>
2022-05-31 22:57:20 +0530

    13th commit

--- Tally-Tkinter.py
+++ Tally-Tkinter.py
@@ -60,7 +60,7 @@ def banking():
     'Arial 7 bold'), anchor='w').place(x=0, y=60, width=1866, height=13)
     home = Label(top, text="", fg='#00c8ff', bg='white', font=(
     'Arial 9 underline'), anchor='w').place(x=1, y=73, width=1309, height=600)
-    b4 = Button(top, text="x", command=home, activeforeground="black", activebackground="#00c8ff",
+    b4 = Button(top, text="x",command=home, activeforeground="black", activebackground="#00c8ff",
             fg='black', bg='#00c8ff', borderwidth=0, font=('Arial 10 bold'),).place(x=1280, y=60,height=12)
 
   
@@ -276,32 +276,6 @@ def create():
     e2 = Entry(top,fg='black',bg='white').place(x = 80, y = 140,width=300,height=30)  
 
    
-def LOGbranchordivision():
-    global Canvas1
-    Canvas1 = Canvas( background="#ffffff", relief="ridge").place(relx=0, rely=0.07, relheight=0.900, relwidth=.850)
-
-    global Canvas2
-    Canvas2 = Canvas( background="#8a8a8a", relief="ridge").place(relx=0, rely=0.07, relheight=0.900, relwidth=.850)
-
-    global Canvas3
-    Canvas3 = Canvas( background="white", relief="ridge").place(relx=0, rely=0.07, relheight=0.500, relwidth=.450)
-    Label1 = Label(Canvas3,text='Name :', background="white",foreground="black",font="-family {Arial} -size 10 -weight bold ").place(relx=0.02, rely=0.09)
-    e1=Entry(Canvas3,bg='#ffdd90').place(relx=0.25,rely=0.09,relwidth=.180)
-    Label2 = Label(Canvas3,text='(alias) :', background="white",foreground="black",font="-family {Arial} -size 10 -weight bold ").place(relx=0.02, rely=0.12)
-    e2=Entry(Canvas3,bg='#ffdd90').place(relx=0.25,rely=0.12,relwidth=.180)
-    Label3 = Label(Canvas3,text='Under :', background="white",foreground="black",font="-family {Arial} -size 10 -weight bold ").place(relx=0.02, rely=0.18)
-    Label4 = Label(Canvas3,text='◆Primary', background="white",foreground="black",font="-family {Arial} -size 10 -weight bold ").place(relx=0.10, rely=0.18)
-    separator = ttk.Separator(Canvas3, orient='horizontal').place(relx=0, rely=0.23, relheight=0, relwidth=0.450)
-    Label5 = Label(Canvas3,text='Nature of Group :', background="white",foreground="black",font="-family {Arial} -size 10 -weight bold ").place(relx=0.02, rely=0.26)
-    Label6 = Label(Canvas3,text='Group behaves like a sub-ledger :', background="white",foreground="black",font="-family {Arial} -size 10 -weight bold ").place(relx=0.02, rely=0.31)
-    Label7 = Label(Canvas3,text='Nett Debit/Credit Balances for Reporting :', background="white",foreground="black",font="-family {Arial} -size 10 -weight bold ").place(relx=0.02, rely=0.36)
-    Label8 = Label(Canvas3,text='Used for calculation (for example: taxes,discounts) :', background="white",foreground="black",font="-family {Arial} -size 10 -weight bold ").place(relx=0.02, rely=0.41)
-    Label9 = Label(Canvas3,text='(for sales invoice entries)', background="white",foreground="black",font="-family {Papyrus} -size 10 -weight bold ").place(relx=0.02, rely=0.46)
-    Label10 = Label(Canvas3,text='Method to allocate when used in purchase invoice :', background="white",foreground="black",font="-family {Arial} -size 10 -weight bold ").place(relx=0.02, rely=0.51)
-    Label11 = Label(Canvas3,text='◆Not Applicable', background="white",foreground="black",font="-family {Arial} -size 10 -weight bold ").place(relx=0.25, rely=0.51)
-    # b1 = Button(Canvas2,text = "Main Location",activeforeground = "black", activebackground = "#ffbe23",fg='black',bg='white',borderwidth=0,font=('Arial 10')).place(relx=0.003, rely=0.13)
-    b2 = Button(Canvas2,text = "X",activeforeground = "white", activebackground = "red",fg='white',bg='black',borderwidth=0,font=('Arial 10'),command=banking).place(relx=0.8, rely=0.1)
-
 
 def checkregister():
     checkregisterr = Label(top, text="Cheque Register", fg='black', bg='#00c8ff', font=(
@@ -445,7 +419,7 @@ def daybook():
             fg='black', bg='#f0f8fc', borderwidth=1, font=('Arial 10  '),anchor='w').place(x=1330, y=63,height=27,width=150)
     side2 = Button(top, text="  Company",activeforeground="black", activebackground="white",
             fg='black', bg='#f0f8fc', borderwidth=1, font=('Arial 10 '),anchor='w').place(x=1330, y=91,height=27,width=150)
-    side3 = Button(top, text="  Voucher Type",activeforeground="black", activebackground="white",
+    side3 = Button(top, text="  Voucher Type", command=vouchertype, activeforeground="black", activebackground="white",
             fg='black', bg='#f0f8fc', borderwidth=1, font=('Arial 10 '),anchor='w').place(x=1330, y=120,height=27,width=150) 
 
     side4 = Button(top, text="  ",activeforeground="black", activebackground="white",
@@ -465,96 +439,113 @@ def daybook():
              fg='black', bg='#f0f8fc', borderwidth=1, font=('Arial 10 '),anchor='w').place(x=1330, y=350,height=27,width=150)
     side12 = Button(top, text="  Change View",activeforeground="black", activebackground="white",
              fg='black', bg='#f0f8fc', borderwidth=1, font=('Arial 10 '),anchor='w').place(x=1330, y=378,height=27,width=150)
-    side12 = Button(top, text="  Exception Reports",activeforeground="black", activebackground="white",
+    side12 = Button(top, text="  Exception Reports", command=exceptionareports,activeforeground="black", activebackground="white",
              fg='black', bg='#f0f8fc', borderwidth=1, font=('Arial 10 '),anchor='w').place(x=1330, y=406,height=27,width=150)
-    side13 = Button(top, text="  Save View",activeforeground="black", activebackground="white",
+    side13 = Button(top, text="  Save View", command=saveview, activeforeground="black", activebackground="white",
              fg='black', bg='#f0f8fc', borderwidth=1, font=('Arial 10 '),anchor='w').place(x=1330, y=434,height=27,width=150)
 
     side14 = Button(top, text="  Configure  ",activeforeground="black", activebackground="white",
              fg='black', bg='#f0f8fc', borderwidth=1, font=('Arial 10 '),anchor='w').place(x=1330, y=758,height=27,width=150)
    
-   
 
-def ledgervouchers():
-    sideheading = Label(top, text="Ledger Vouchers", fg='black', bg='#00c8ff', font=(
+def vouchertype():
+    paymentadvic = Label(top, text="Select item", fg='black', bg='#00c8ff', font=(
     'Arial 7 bold'), anchor='w').place(x=1, y=60, width=1219, height=13)
-    sideheadingcolor = Label(top, text="", fg='#00c8ff', bg='white', font=(
-    'Arial 9 underline'), anchor='w').place(x=1, y=73, width=1219, height=604)
+    paymentadviceform = Label(top, text="", fg='#00c8ff', bg='white', font=(
+    'Arial 9 underline'), anchor='w').place(x=1, y=73, width=1298, height=604)
+    b4 = Button(top, text="x", command=daybook, activeforeground="black", activebackground="#00c8ff",
+            fg='black', bg='#00c8ff', borderwidth=0, font=('Arial 10 bold'),).place(x=1280, y=60,height=12)
+
+    Label1 = Label(top,text='Name of Voucher Type',borderwidth="0", width=3, background="#faf8d7",
+                                     foreground="#00254a",
+                                     font="-family {Segoe UI} -size 10 -weight bold ",anchor="n",bd=2,)
+    Label1.place(relx=0.35, rely=0.09, relheight=0.10, relwidth=0.150)
+    Entry1 = Entry(top,width=8,borderwidth="3",bg="#f7d065")
+    Entry1.place(relx=0.36, rely=0.14, relheight=0.03, relwidth=0.132)
+
+
+    name = Label(top, fg='#00c8ff', bg='#94ecf7', borderwidth=2, font=(
+    'Arial 9 underline'), anchor='w').place(x=1300, y=60, width=315, height=900)
+
+    menu = Label(top, fg='#00c8ff', bg='#a9ceeb', borderwidth=2, font=(
+        'Arial 9 underline'), anchor='w').place(x=504, y=180, width=300, height=400)
+
+    menuname = Label(top,text="Voucher Types", fg='white', bg='#0851a8', borderwidth=2, font=(
+        'Arial 9 '), anchor='center').place(x=504, y=160, width=300, height=19)
+
+
+
+    b9  = Button(top,text = "Create",command=create,activeforeground = "black", activebackground = "#ffbe23",bg='#a9ceeb',borderwidth=0,font=('Arial 10')).place(relx=0.450, rely=0.220,relwidth=0.070,anchor="nw")
+    b10 = Button(top,text = "◆All items",command=paymentadvice,activeforeground = "black", activebackground = "#ffbe23",bg='#a9ceeb',borderwidth=0,font=('Arial 10'),anchor="w").place(relx=0.350, rely=0.250,relwidth=.148)
+    b11 = Button(top,text = "Contra",command=paymentadvice,activeforeground = "black", activebackground = "#ffbe23",bg='#a9ceeb',borderwidth=0,font=('Arial 10'),anchor="w").place(relx=0.350, rely=0.280,relwidth=.148)
+    b12 = Button(top,text = "Credit Note",command=paymentadvice,activeforeground = "black", activebackground = "#ffbe23",bg='#a9ceeb',borderwidth=0,font=('Arial 10'),anchor="w").place(relx=0.350, rely=0.310,relwidth=.148)
+    b13 = Button(top,text = "Debit Note",command=paymentadvice,activeforeground = "black", activebackground = "#ffbe23",bg='#a9ceeb',borderwidth=0,font=('Arial 10'),anchor="w").place(relx=0.350, rely=0.340,relwidth=.148)
+    b14 = Button(top,text = "Journal",command=paymentadvice,activeforeground = "black", activebackground = "#ffbe23",bg='#a9ceeb',borderwidth=0,font=('Arial 10'),anchor="w").place(relx=0.350, rely=0.370,relwidth=.148)
+    b15 = Button(top,text = "Memorandum",command=paymentadvice,activeforeground = "black", activebackground = "#ffbe23",bg='#a9ceeb',borderwidth=0,font=('Arial 10'),anchor="w").place(relx=0.350, rely=0.400,relwidth=.148)
+    b16 = Button(top,text = "Payment",command=paymentadvice,activeforeground = "black", activebackground = "#ffbe23",bg='#a9ceeb',borderwidth=0,font=('Arial 10'),anchor="w").place(relx=0.350, rely=0.430,relwidth=.148)
+    b17 = Button(top,text = "Physical Stock",command=paymentadvice,activeforeground = "black", activebackground = "#ffbe23",bg='#a9ceeb',borderwidth=0,font=('Arial 10'),anchor="w").place(relx=0.350, rely=0.460,relwidth=.148)
+    b18 = Button(top,text = "Purchase",command=paymentadvice,activeforeground = "black", activebackground = "#ffbe23",bg='#a9ceeb',borderwidth=0,font=('Arial 10'),anchor="w").place(relx=0.350, rely=0.490,relwidth=.148)
+    b19 = Button(top,text = "Receipt",command=paymentadvice,activeforeground = "black", activebackground = "#ffbe23",bg='#a9ceeb',borderwidth=0,font=('Arial 10'),anchor="w").place(relx=0.350, rely=0.520,relwidth=.148)
+    b20 = Button(top,text = "Reversing Journal",command=paymentadvice,activeforeground = "black", activebackground = "#ffbe23",bg='#a9ceeb',borderwidth=0,font=('Arial 10'),anchor="w").place(relx=0.350, rely=0.550,relwidth=.148)
+    b21 = Button(top,text = "Sales",command=paymentadvice,activeforeground = "black", activebackground = "#ffbe23",bg='#a9ceeb',borderwidth=0,font=('Arial 10'),anchor="w").place(relx=0.350, rely=0.580,relwidth=.148)
+    b22 = Button(top,text = "Stock Journal",command=paymentadvice,activeforeground = "black", activebackground = "#ffbe23",bg='#a9ceeb',borderwidth=0,font=('Arial 10'),anchor="w").place(relx=0.350, rely=0.610,relwidth=.148)
+
+
+def exceptionareports():
+    paymentadvic = Label(top, text="Select item", fg='black', bg='#00c8ff', font=(
+    'Arial 7 bold'), anchor='w').place(x=1, y=60, width=1219, height=13)
+    paymentadviceform = Label(top, text="", fg='#00c8ff', bg='white', font=(
+    'Arial 9 underline'), anchor='w').place(x=1, y=73, width=1298, height=604)
+    b4 = Button(top, text="x", command=daybook, activeforeground="black", activebackground="#00c8ff",
+            fg='black', bg='#00c8ff', borderwidth=0, font=('Arial 10 bold'),).place(x=1280, y=60,height=12)
+
+    Label1 = Label(top,text='Exceptions Reports',borderwidth="0", width=3, background="#faf8d7",
+                                     foreground="#00254a",
+                                     font="-family {Segoe UI} -size 10 -weight bold ",anchor="n",bd=2,)
+    Label1.place(relx=0.35, rely=0.09, relheight=0.10, relwidth=0.150)
+    Entry1 = Entry(top,width=8,borderwidth="3",bg="#f7d065")
+    Entry1.place(relx=0.36, rely=0.14, relheight=0.03, relwidth=0.132)
+
+
+    name = Label(top, fg='#00c8ff', bg='#94ecf7', borderwidth=2, font=(
+    'Arial 9 underline'), anchor='w').place(x=1300, y=60, width=315, height=900)
+
+    menu = Label(top, fg='#00c8ff', bg='#a9ceeb', borderwidth=2, font=(
+        'Arial 9 underline'), anchor='w').place(x=504, y=180, width=300, height=400)
+
+    menuname = Label(top,text="List of Exception Reports", fg='white', bg='#0851a8', borderwidth=2, font=(
+        'Arial 9 '), anchor='center').place(x=504, y=160, width=300, height=19)
+
+
+
+    b9 = Button(top,text = "Show More",command=create,activeforeground = "black", activebackground = "#ffbe23",bg='#a9ceeb',borderwidth=0,font=('Arial 10')).place(relx=0.450, rely=0.220,relwidth=0.070,anchor="nw")
+    b10 = Button(top,text = "Optional Vouchers",command=paymentadvice,activeforeground = "black", activebackground = "#ffbe23",bg='#a9ceeb',borderwidth=0,font=('Arial 10'),anchor="w").place(relx=0.350, rely=0.250,relwidth=.148)
+    b10 = Button(top,text = "Cancelled Vouchers",command=paymentadvice,activeforeground = "black", activebackground = "#ffbe23",bg='#a9ceeb',borderwidth=0,font=('Arial 10'),anchor="w").place(relx=0.350, rely=0.280,relwidth=.148)
+    b10 = Button(top,text = "Post-Dated Vouchers",command=paymentadvice,activeforeground = "black", activebackground = "#ffbe23",bg='#a9ceeb',borderwidth=0,font=('Arial 10'),anchor="w").place(relx=0.350, rely=0.310,relwidth=.148)
+
+def saveview():
+    saveview = Label(top, text="Day Book", fg='black', bg='#00c8ff', font=(
+    'Arial 7 bold'), anchor='w').place(x=1, y=60, width=1219, height=13)
+    checkregisterform = Label(top, text="", fg='#00c8ff', bg='white', font=(
+    'Arial 9 underline'), anchor='w').place(x=1, y=73, width=1298, height=804)
+    b4 = Button(top, text="x", command=daybook, activeforeground="black", activebackground="#00c8ff",
+            fg='black', bg='#00c8ff', borderwidth=0, font=('Arial 10 bold'),).place(x=1280, y=60,height=12)
     
-    b4 = Button(top, text="x", command=ledgermonthlysummary, activeforeground="black", activebackground="#00c8ff",
-            fg='black', bg='#00c8ff', borderwidth=0, font=('Arial 10 bold'),).place(x=1204, y=60,height=12)
-
-    global selected_ledgers_frame
-    selected_ledgers_frame=Frame(top,bg="white",relief=RAISED,bd=0)
-    selected_ledgers_frame.place(x=1, y=73,width=1219, height=504)
+    global Canvas2
+    Canvas2 = Canvas( background="#8a8a8a", relief="ridge").place(relx=0, rely=0.085, relheight=0.900, relwidth=.850)
     
-    f11=Frame(selected_ledgers_frame,bg="white",relief=RAISED,bd=0.5)
-    f11.grid(row=1,column=0,columnspan=3,ipadx=410,ipady=5)
-    l1f1=Label(f11,text="    Ledger :",font=("Arial",11),fg="black",bg="white",anchor="w", borderwidth=0,relief=GROOVE,width=44,height=1)
-    l1f1.pack(fill=X,pady=2,padx=2)
+    global Canvas3
+    Canvas3 = Canvas( background="white", relief="ridge").place(relx=0.25, rely=0.35, relheight=0.400, relwidth=0.420)
 
-    f111=Frame(selected_ledgers_frame,bg="white",relief=RAISED)
-    f111.place(x=75,y=3,width=100,height=23)
-    l1f2=Label(f111,text=" Ledger name",font=("Arial",10,"bold"),bg="white",fg="black",)
-    l1f2.place(x=40,y=10,anchor="center")
+    Head = Label(Canvas3,text='Save View', background="white",foreground="black",font="-family {Arial} -size 10 -weight bold ").place(relx=0.40, rely=0.37)
+    separator = ttk.Separator(Canvas3, orient='horizontal').place(relx=0.40, rely=0.40, relheight=0, relwidth=0.05)
+    Label1 = Label(Canvas3,text='Name :', background="white",foreground="black",font="-family {Arial} -size 10 -weight bold ").place(relx=0.30, rely=0.43)
+    e1=Entry(Canvas3,bg='#ffdd90').place(relx=0.50,rely=0.43,relwidth=0.15)
+    Label2 = Label(Canvas3,text='Set this as default view for the report:', background="white",foreground="black",font="-family {Arial} -size 10 -weight bold ").place(relx=0.30, rely=0.46)
+    combo2 = ttk.Combobox(Canvas3,values=["Yes", "No"]).place(relx=0.50,rely=0.47)
+    Label8 = Label(Canvas3,text='Show additional configuration:', background="white",foreground="black",font="-family {Arial} -size 10 -weight bold ").place(relx=0.30, rely=0.49)
+    combo3 = ttk.Combobox(Canvas3,values=["Yes", "No"]).place(relx=0.50, rely=0.50)
     
-    l1f2=Label(top,text="April 1-2020 to April 1-2021",font=("Arial",10,"bold"),bg="white",fg="black")
-    l1f2.place(x=1120,y=89,anchor="center")
-    
-
-    t3 = ttk.Treeview(top)
-    t3['columns']=('Date','Particulars','Vch Type','Vch No.','Debit','Credit')
-    t3.column('#0', width=0, stretch=NO)
-    t3.column('Date', anchor=W, width=100,minwidth=100)
-    t3.column('Particulars', anchor=W, width=405,minwidth=425)
-    t3.column('Vch Type', anchor=CENTER, width=125,minwidth=125)
-    t3.column('Vch No.', anchor=CENTER, width=125,minwidth=125)
-    t3.column('Debit', anchor=CENTER, width=125,minwidth=125)
-    t3.column('Credit', anchor=CENTER, width=125,minwidth=125)
-    
-    separator = ttk.Separator(top, orient='horizontal')
-    separator.place(relx=0.00, rely=0.19, relheight=0, relwidth=0.893)
-
-    t3.heading('#0', text='', anchor=CENTER)
-    t3.heading('Date', text='Date', anchor=W)
-    t3.heading('Particulars', text='Particulars', anchor=W, )
-    t3.heading('Vch Type', text='Vch Type', anchor=CENTER)
-    t3.heading('Vch No.', text='Vch No.', anchor=CENTER)
-    t3.heading('Debit', text='Debit', anchor=CENTER)
-    t3.heading('Credit', text='Credit', anchor=CENTER)
-   
-    t3.insert(parent='', index=0, iid=0, text='', values=('-----','-','-','-','-','-'))
-    t3.place(x=0, y=107, height=569, width=1220)
-
-
-    f18=Frame(t3,bg="white",relief=RAISED,bd=1)
-    f18.place(x=0,y=500,width=1220,height=40)
-    l5f6=Label(f18,text=" Opening Balance :",font=("Arial",9),bg="white",fg="black",borderwidth=0)
-    l5f6.place(x=200,y=0,)
-
-    l5f6s=Label(f18,text=" 4564545.00",font=("Arial",9,"bold"),bg="white",fg="black",borderwidth=0)
-    l5f6s.place(x=1100,y=0,)
-    l5f7=Label(f18,text="        Current Tottal :",font=("Arial",9),bg="white",fg="black",borderwidth=0)
-    l5f7.place(x=200,y=18,)
-
-    f19=Frame(selected_ledgers_frame,bg="white",relief=RAISED,bd=1)
-    f19.place(x=874,y=798,width=120,height=20)
-    l6f6=Label(f19,text="544650.00",font=("Arial",10,"bold"),bg="white",fg="black",borderwidth=0)
-    l6f6.place(x=0,y=0,anchor="nw")
-
-    f20=Frame(selected_ledgers_frame,bg="white",relief=RAISED,bd=1)
-    f20.place(x=994,y=798,width=120,height=20)
-    l7f6=Label(f20,text="544650.00",font=("Arial",10,"bold"),bg="white",fg="black",borderwidth=0)
-    l7f6.place(x=0,y=0,anchor="nw")
-
-    f21=Frame(selected_ledgers_frame,bg="white",relief=RAISED,bd=1)
-    f21.place(x=1114,y=98,width=104,height=20)
-    l7f6=Label(f21,text="452650.00",font=("Arial",10,"bold"),bg="white",fg="black",borderwidth=0)
-    l7f6.place(x=0,y=0,anchor="nw")
-
-
-
-
 
 
 # NavBar Start
